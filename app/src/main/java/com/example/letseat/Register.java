711495d646fd1460ea04d7e20d4323274f492915
Li-Haowei <haoweilee666@gmail.com>
2022-04-16 19:45:22 -0400

    added a new class that asks user to input their information before account creation

--- app/src/main/java/com/example/letseat/Register.java
+++ app/src/main/java/com/example/letseat/Register.java
@@ -182,6 +182,7 @@ public class Register extends AppCompatActivity {
                         }
                     }
                 });
+                startActivity(new Intent(getApplicationContext(),UserPref.class));
             }
 
         });
