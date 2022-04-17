f79f00ec465d859dd6b85fc7820c6bce5de846fd
Hsueh-Yuan Chou <maxchou@bu.edu>
2022-04-16 22:25:26 -0400

    "integrated sendbird into app in the latest version, also able to create channel with code"

--- app/src/main/java/com/example/letseat/ChatActivity.java
+++ app/src/main/java/com/example/letseat/ChatActivity.java
@@ -1,14 +1,58 @@
 package com.example.letseat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+import com.sendbird.android.GroupChannel;
+import com.sendbird.android.GroupChannelListQuery;
+import com.sendbird.uikit.fragments.ChannelListFragment;
+
 
 public class ChatActivity extends AppCompatActivity {
+    ImageView btn_back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        btn_back = (ImageView) findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        ChannelListFragment Fragment = createChannelListFragment();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.channelList_frag, Fragment, null)
+                    .commit();
+        }
+    }
+
+    protected ChannelListFragment createChannelListFragment() {
+
+        GroupChannelListQuery query = GroupChannel.createMyGroupChannelListQuery();
+        query.setIncludeEmpty(true);
+        query.setOrder(GroupChannelListQuery.Order.CHRONOLOGICAL);
+
+        return new ChannelListFragment.Builder()
+                .setUseHeader(false)
+                .setUseHeaderLeftButton(true)
+                .setGroupChannelListQuery(query)
+                .build();
     }
 }
+
--- app/src/main/java/com/example/letseat/widgets/SendBirdBaseApp.java
+++ app/src/main/java/com/example/letseat/widgets/SendBirdBaseApp.java
@@ -1,2 +1,93 @@
-package com.example.letseat.widgets;public class SendBirdBaseApp {
+package com.example.letseat.widgets;
+
+import android.app.Application;
+import android.util.Log;
+
+
+import com.sendbird.android.OpenChannel;
+import com.sendbird.android.SendBird;
+import com.sendbird.android.SendBirdException;
+import com.sendbird.android.User;
+import com.sendbird.android.handlers.InitResultHandler;
+import com.sendbird.uikit.SendBirdUIKit;
+import com.sendbird.uikit.adapter.SendBirdUIKitAdapter;
+import com.sendbird.uikit.interfaces.UserInfo;
+
+public class SendBirdBaseApp extends Application {
+    String App_ID = "44E15C0D-8F8C-46E1-AE5E-746D63CB7619";
+    String User_ID ;
+    String User_Nickname ;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //To integrate and run Sendbird UIKit with user information in our application,
+        // we need to initialize an UIkit instance first.
+
+        //According to the documentation, we can initialize an instance by passing
+        // the sendBirdUIKitAdapter instance as an argument to a parameter in the
+        // SendBirdUIKit.init() method.
+        SendBirdUIKit.init(new SendBirdUIKitAdapter() {
+            @Override
+            public String getAppId() {
+                return App_ID;  // Specify your Sendbird application ID.
+            }
+
+            @Override
+            public String getAccessToken() {
+                return "";
+            }
+
+            @Override
+            public UserInfo getUserInfo() {
+                return new UserInfo() {
+                    @Override
+                    public String getUserId() {
+                        return User_ID;
+                    }
+
+                    @Override
+                    public String getNickname() {
+                        return User_Nickname;
+                    }
+
+                    @Override
+                    public String getProfileUrl() {
+                        return "";
+                    }
+                };
+            }
+
+            @Override
+            public InitResultHandler getInitResultHandler() {
+                return new InitResultHandler() {
+                    @Override
+                    public void onMigrationStarted() {
+                        // DB migration has started.
+                    }
+
+                    @Override
+                    public void onInitFailed(SendBirdException e) {
+                        // If DB migration fails, this method is called.
+                    }
+
+                    @Override
+                    public void onInitSucceed() {
+                        // If DB migration is successful, this method is called and you can proceed to the next step.
+                        // In the sample app, the `LiveData` class notifies you on the initialization progress
+                        // And observes the `MutableLiveData<InitState> initState` value in `SplashActivity()`.
+                        // If successful, the `LoginActivity` screen
+                        // Or the `HomeActivity` screen will show.
+                    }
+                };
+            }
+        }, this);
+
+    }
+
+    //    Setter methods inside the BaseApplication class that will be be called in the loginActivity.
+    public void setUserId(String userId) {this.User_ID = userId;}
+
+    public void setUserNickname(String userNickname){this.User_Nickname = userNickname;}
+
 }
